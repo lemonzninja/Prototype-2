4a8a4e8e42d3de03df0fb7a479baedc57482a46b
Holographic <budninja36@gmail.com>
2023-08-01 12:45:02 -0400

    Add new prefabs and materials for challenge 2
    Added three new ball prefabs for "Challenge 2". Each prefab has different parameters set for speed, scale, and coloring. Different materials such as "Blue" have also been created for enhancement of visual identification. Each of these modifications is vital for creating gameplay diversity and to visually distinguish between the various types of ball prefabs. Additional prefabs and materials are also added to "_Source_Files" folder in "Challenge 2" assets.

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+    
+    private float minSpawnInterval = 3.0f;
+    private float maxSpawnInterval = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(ScheduleNextSpawn), startDelay);
+    }
+
+    private void ScheduleNextSpawn()
+    {
+        float spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+        Invoke(nameof(SpawnRandomBall), spawnInterval);
+    }
+    
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        
+        // Generate a random index between 0 and the length of the array
+        int index = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
+        
+        // Schedule next spawn
+        ScheduleNextSpawn();
+    }
+}
