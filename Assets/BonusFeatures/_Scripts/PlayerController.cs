a9c74053648513919000c3e0337f33ec821260ec
Holographic <budninja36@gmail.com>
2023-08-06 14:03:10 -0400

    cant move out of the left and right of screen

--- Assets/BonusFeatures/_Scripts/PlayerController.cs
+++ Assets/BonusFeatures/_Scripts/PlayerController.cs
@@ -1,82 +1,55 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // The projectile prefab
-	[SerializeField] private GameObject projectilePrefab;
-	
-	// player movement variables
-	public float horizontalInput;
-	public float verticalInput;
-	
-	[SerializeField] private float speed = 10.0f;
+    [SerializeField] public float moveSpeed = 10.0f;
+    [SerializeField] private float ScreenBoundaryPos;
 
-    // The left and right boundaries for the player
-	[SerializeField] private float xPosMin = 0.0f;
-	[SerializeField] private float xPosMax = 0.0f;
-	// The top and bottom boundaries for the player    
-	[SerializeField] private float zPosMin = 0.0f;
-	[SerializeField] private float zPosMax = 0.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    
+    private Vector3 _playerPos;
+    private Vector3 _position;
+    private Transform _transform;
 
-	private void Update()
-	{
-		MovePlayer();
-		KeepPlayerInBounds();
-	}
-	
-	private void MovePlayer()
-	{
-		// Move the player left and right
-		horizontalInput = Input.GetAxis("Horizontal");
-		transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
-		
-		// Move the player forward and backward
-		verticalInput = Input.GetAxis("Vertical");
-		transform.Translate(Vector3.forward * (verticalInput * Time.deltaTime * speed));
-	}
+    private void Start()
+    {
+        _transform = transform;
+        _position = transform.position;
+    }
 
-	private void KeepPlayerInBounds()
-	{
-		/*
-		 * Keep the player in bounds
-		 */
-		
-		// Keep the player form going off the bottom of the screen
-		if (transform.position.x < xPosMin)
-		{
-			var transform1 = transform;
-			var position = transform1.position;
-			
-			position = new Vector3(xPosMin, position.y, position.z);
-			transform1.position = position;
-		}
-		// Keep the player form going off the top of the screen
-		if (transform.position.x > xPosMax)
-		{
-			var transform1 = transform;
-			var position = transform1.position;
-			
-			position = new Vector3(xPosMax, position.y, position.z);
-			transform1.position = position;
-		}
-        // Keep the player form going off the left of the screen
-		if (transform.position.y < zPosMin)
-		{
-			var transform1 = transform;
-			var position = transform1.position;
-			
-			position = new Vector3(position.x, position.y, zPosMin);
-			transform1.position = position;
-		}
-		// Keep the player form going off the right of the screen
-		if (transform.position.y > zPosMax)
-		{
-			var transform1 = transform;
-			var position = transform1.position;
-			
-			position = new Vector3(position.x, position.y, zPosMax);
-			transform1.position = position;
-		}
-	}
+    private void Update()
+    {
+        MovePlayer();
+        KeepPlayerOnScreen();
+    }
+
+    private void MovePlayer()
+    {
+        // Get left/right input.
+        horizontalInput = Input.GetAxis("Horizontal");
+        _position += Vector3.right * (horizontalInput * moveSpeed * Time.deltaTime);
+        
+        // Get up/down input.
+        verticalInput = Input.GetAxis("Vertical");
+        _position += Vector3.forward * (verticalInput * moveSpeed * Time.deltaTime);
+        
+        _transform.position = _position;
+    }
+    
+    private void KeepPlayerOnScreen()
+    {
+        /*
+         * keep player object on screen.
+         */
+        
+        // Get player position.
+        _playerPos = _position;
+        // Clamp the player position to the screen boundaries.
+        var xPos = Mathf.Clamp(_playerPos.x, -ScreenBoundaryPos, ScreenBoundaryPos);
+        // Set the player position.
+        _playerPos = new Vector3(xPos, _position.y, _position.z);
+        _position = _playerPos;
+        
+    }
 }
